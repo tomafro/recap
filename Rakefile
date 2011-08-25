ca99bbff1bab3a3941a8e17b2cfc6639bdb8006a
Tom Ward <tom@popdog.net>
2011-08-25 13:42:31 +0100

    Updated index, added licence, and added rake task to publish docs

--- Rakefile
+++ Rakefile
@@ -3,3 +3,10 @@ require 'rocco/tasks'
 
 desc 'build docs'
 Rocco::Task.new :rocco, 'doc/', ['index.rb', 'lib/tomafro/**/*.rb']
+
+desc 'publish docs'
+task :publish do
+  sha = `git ls-tree -d HEAD doc | awk '{print $3}'`.strip
+  commit = `echo "Publishing docs from master branch" | git commit-tree #{sha} -p refs/heads/gh-pages`.strip
+  `git update-ref refs/heads/gh-pages #{commit}`
+end
--- index.rb
+++ index.rb
@@ -4,3 +4,6 @@
 # Most of the documentation can be found with the [main recipe](lib/tomafro/deploy.html), while the
 # [bundler](lib/tomafro/deploy/bundler.html) and [foreman](lib/tomafro/deploy/foreman.html) recipes document
 # their extensions.
+
+# The code is available [on github](http://github.com/tomafro/tomafro-deploy) and released under the
+# [MIT License](http://github.com/tomafro/tomafro-deploy/LICENSE)
