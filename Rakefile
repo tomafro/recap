756c9d957b0d6f5085fe9c8f41c218bf72be19ae
Tom Ward <tom@popdog.net>
2011-08-25 09:19:27 +0100

    Move index documentation to index.rb, and introduce a rake task to generate docs

--- Rakefile
+++ Rakefile
@@ -1,1 +1,5 @@
 require 'bundler/gem_tasks'
+require 'rocco/tasks'
+
+desc 'build docs'
+Rocco::Task.new :rocco, 'doc/', ['index.rb', 'lib/tomafro/**/*.rb']
---
+++ index.rb
@@ -0,0 +1,6 @@
+# A simple, opinionated set of deployment recipes for capistrano, using git, bundler and foreman to
+# manage application deployments.
+
+# Most of the documentation can be found with the [main recipe](lib/tomafro/deploy.html), while the
+# [bundler](lib/tomafro/deploy/bundler.html) and [foreman](lib/tomafro/deploy/foreman.html) recipes document
+# their extensions.
