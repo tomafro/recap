569359781e92fc666fd01997ab9faaf2ea6c07b8
Tom Ward <tom@popdog.net>
2011-09-15 17:15:30 +0100

    And now rails has a barely working recipe, full of migration goodness.

--- lib/tomafro/deploy/rails.rb
+++ lib/tomafro/deploy/rails.rb
@@ -1,25 +1,22 @@
 require 'tomafro/deploy'
 
 Capistrano::Configuration.instance(:must_exist).load do
-  # To use this recipe, both the application's name and its git repository are required.
-  set(:rails_env, "production")
-
-  def rails_rake(cmd)
-    "RAILS_ENV=#{rails_env} cd #{deploy_to} && ./bin/rake #{cmd}"
-  end
-
   namespace :rails do
     namespace :db do
-      task :load do
-        rails_rake 'db:schema:load'
+      task :load_schema do
+        if deployed_file_exists?("db/schema.rb")
+          as_app './bin/rake db:create db:schema:load'
+        end
       end
 
       task :migrate do
-        rails_rake 'db:migrate'
+        if deployed_file_changed?("db/schema.rb")
+          as_app './bin/rake db:migrate'
+        end
       end
     end
   end
 
-  after "deploy:clone_code", "rails:db:load"
-  after "deploy:migrate", "rails:db:migrate"
+  after "deploy:clone_code", "rails:db:load_schema"
+  after "deploy:update_code", "rails:db:migrate"
 end
