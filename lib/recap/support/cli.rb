ed594578f88f1157bb8f4a69066406404abe316b
Tom Ward <tom@popdog.net>
2012-05-01 11:58:54 +0100

    Make recap CLI more friendly
    Update the CLI command to guess the appropriate recap variant (static/ruby/rails) to add to the Capfile, and add a server line (without which deployment will fail!).

--- lib/recap/support/cli.rb
+++ lib/recap/support/cli.rb
@@ -4,18 +4,23 @@ module Recap::Support
   class CLI < Thor
     include Thor::Actions
 
-    attr_accessor :name, :repository
+    attr_accessor :name, :repository, :type, :server
 
     def self.source_root
       File.expand_path("../templates", __FILE__)
     end
 
     desc 'setup', 'Setup basic capistrano recipes, e.g: recap setup'
-    method_option :name, :aliases => "-n"
-    method_option :repository, :aliases => "-r"
+    method_option :name
+    method_option :repository
+    method_option :server
+    method_option :type, :type => 'string', :banner => 'static|ruby|rails'
+
     def setup
       self.name = options["name"] || guess_name
       self.repository = options["repo"] || guess_repository
+      self.type = options["type"] || guess_type
+      self.server = options["server"] || 'your-server-address'
       template 'Capfile.erb', 'Capfile'
     end
 
@@ -27,6 +32,18 @@ module Recap::Support
 
     def guess_repository
       `git remote -v`.split[1]
+    end
+
+    def guess_type
+      if File.exist?('Gemfile.lock')
+        if File.read('Gemfile.lock') =~ / rails /
+          'rails'
+        else
+          'ruby'
+        end
+      else
+        'static'
+      end
     end
   end
 end
