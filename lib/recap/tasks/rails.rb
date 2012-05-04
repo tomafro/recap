eff9c6ec943ddc8856051de78bc6a5ebc65b15d2
Tom Ward <tom@popdog.net>
2012-05-04 13:07:41 +0100

    Add some documentation for the rails tasks.

--- lib/recap/tasks/rails.rb
+++ lib/recap/tasks/rails.rb
@@ -1,3 +1,6 @@
+# The rails tasks add to the standard deployment with tasks to support running
+# database migrations and precompiling assets.
+
 require 'recap/tasks'
 
 module Recap::Tasks::Rails
@@ -18,13 +21,22 @@ module Recap::Tasks::Rails
       end
     end
 
+    # The `rails:assets:precompile` runs rails' asset precompilation rake task on
+    # the server.  As assets come from so many sources (app/assets, vendor/assets
+    # and from individual gems) it's not easy to determine whether compilation is
+    # required, so it is done on every deploy.
     namespace :assets do
       task :precompile do
         as_app "./bin/rake RAILS_GROUPS=assets assets:precompile"
       end
     end
 
+    # After the code is first cloned (during `deploy:setup`) load the schema into
+    # the database.
     after "deploy:clone_code", "rails:db:load_schema"
+
+    # On every deploy, after the code is updated, run the database migrations
+    # and precompile the assets.
     after "deploy:update_code", "rails:db:migrate", "rails:assets:precompile"
   end
 end
