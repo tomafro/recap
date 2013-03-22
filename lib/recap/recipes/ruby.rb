e51d3a067ad06f29510523205accfe7ea5c642c2
Tom Ward <tom@popdog.net>
2013-03-22 23:21:44 +0000

    Warn user if rails tasks aren't loaded for a rails project.

--- lib/recap/recipes/ruby.rb
+++ lib/recap/recipes/ruby.rb
@@ -1,6 +1,7 @@
 # Require `recap/recipes/ruby` in your `Capfile` to use the default recap recipies for deploying a
 # Ruby application.
 require 'recap/tasks/deploy'
+require 'recap/tasks/ruby'
 
 # If your application uses Bundler, `bundle install` will be run automatically when deploying
 # any changes to your `Gemfile`.
---
+++ lib/recap/tasks/ruby.rb
@@ -0,0 +1,23 @@
+module Recap::Tasks::Ruby
+  extend Recap::Support::Namespace
+
+  namespace :ruby do
+    set(:skip_rails_recipe_not_used_warning, false)
+
+    task :preflight do
+      if exit_code("grep rails #{deploy_to}/Gemfile") == "0"
+        unless skip_rails_recipe_not_used_warning || Recap::Tasks.const_defined?(:Rails)
+          logger.important %{
+Warning: it looks like you're using the recap ruby recipe on a rails project.
+This will prevent some rails specific features such as asset compilation and
+database migrations from working correctly.
+To fix this, require 'recap/recipes/rails' from within your Capfile.  To
+suppress this warning, set :skip_rails_recipe_not_used_warning to true.
+}
+        end
+      end
+    end
+
+    after "preflight:check", "ruby:preflight"
+  end
+end
