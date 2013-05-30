129f536c69e4dbaeb335042c2463e4612f9bfb5d
Tom Ward <tom@popdog.net>
2013-05-30 16:24:03 +0100

    Some ssh tests

--- spec/models/cli_spec.rb
+++ spec/models/cli_spec.rb
@@ -3,20 +3,23 @@ require 'spec_helper'
 describe Recap::Support::CLI do
   subject { Recap::Support::CLI.new }
 
-  describe "#setup" do
-    it 'determines the git repository URL' do
-      Recap::Support::ShellCommand.stubs(:execute).with('git config --get remote.origin.url').returns(%{git@github.com:freerange/recap.git})
-      subject.stubs(:template)
-      subject.setup
-      subject.repository.should eql('git@github.com:freerange/recap.git')
-    end
+  describe 'ssh' do
 
-    it 'handles exception when no git repository present and uses <unkown>' do
-      Recap::Support::ShellCommand.stubs(:execute).with('git config --get remote.origin.url').raises
-      subject.stubs(:template)
-      subject.expects(:warn)
-      lambda { subject.setup }.should_not raise_error
-      subject.repository.should eql('<unknown>')
-    end
+    p `env`
+
+    File.write('recap-ssh-config', %{
+Host default
+  HostName 127.0.0.1
+  Port 22
+  UserKnownHostsFile /dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  IdentitiesOnly yes
+  LogLevel FATAL
+      })
+
+    p `ssh -F recap-ssh-config env`
+    p `ssh 127.0.0.1 env`
+
   end
 end
