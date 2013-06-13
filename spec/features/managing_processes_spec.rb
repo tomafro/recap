c108291f7567b51d509c80dfecb033ba8e0d73e0
Tom Ward <tom@popdog.net>
2013-06-13 07:51:48 +0100

    Mighty white

--- spec/features/managing_processes_spec.rb
+++ spec/features/managing_processes_spec.rb
@@ -25,7 +25,8 @@ describe 'managing processes' do
     puts server.run('which bundle')
     puts server.run('which ruby')
     puts server.run('which gem')
-    puts server.run('ls /opt/ruby/bin')
+    puts server.run('ruby --version')
+    puts server.run('gem --version')
 
 
 
